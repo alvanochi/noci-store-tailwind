f91cea68d01ac791393bedcddd912b2c7bba67ea
= <=>
2023-08-31 02:12:05 +0700

    adding hover scale and centering card

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,12 +2,14 @@ const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
+  hamburger.classList.toggle("border-[1px]");
   nav.classList.toggle("hidden");
 });
 
 window.addEventListener("click", (e) => {
   if (e.target != hamburger && e.target != nav) {
     hamburger.classList.remove("hamburger-active");
+    hamburger.classList.toggle("border-[1px]");
     nav.classList.add("hidden");
   }
 });
