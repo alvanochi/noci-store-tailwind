2a0dbb3d2f3431eebb129b5f2bbf39019e707b9b
= <=>
2023-09-01 17:26:17 +0700

    fixing center for mobile and slide down for hamburger

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,19 @@
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav-menu");
+const header = document.querySelector("header");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("hamburger-active");
   hamburger.classList.toggle("border-[1px]");
   nav.classList.toggle("hidden");
+  header.classList.remove("transparent");
 });
 
 window.addEventListener("click", (e) => {
+  if (hamburger.classList.contains("border-[1px]") && e.target != hamburger) {
+    return;
+  }
+
   if (e.target != hamburger && e.target != nav) {
     hamburger.classList.remove("hamburger-active");
     hamburger.classList.toggle("border-[1px]");
@@ -15,12 +22,18 @@ window.addEventListener("click", (e) => {
 });
 
 window.onscroll = function () {
-  const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
 
-  if (window.pageYOffset > fixedNav) {
+  if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    header.classList.add("transparent");
+    hamburger.classList.remove("hamburger-active");
+    nav.classList.add("hidden");
+    if (!hamburger.classList.contains("border-[1px]")) {
+      hamburger.classList.add("border-[1px]");
+    }
   } else {
     header.classList.remove("navbar-fixed");
+    hamburger.classList.add("border-[1px]");
   }
 };
